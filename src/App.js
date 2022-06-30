8c6b40ced2c013fd6434ec946aea9180e2a02dc2
ghost <michealj13402@gmail.com>
2022-06-29 22:24:03 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Dropdown, Menu, Button } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const { Header, Content } = Layout;
+
+const App = () => {
+  const [authed, setAuthed] = useState(false);
+
+  useEffect(() => {
+    const authToken = localStorage.getItem("authToken");
+    if (authToken !== undefined) {
+      setAuthed(true);
+    }
+  }, []);
+
+  // const handleLoginSuccess = (token, asHost) => {
+  //   localStorage.setItem("authToken", token);
+  //   localStorage.setItem("asHost", asHost);
+  //   this.setState({
+  //     authed: true,
+  //     asHost,
+  //   });
+  // };
+
+  // const handleLogOut = () => {
+  //   localStorage.removeItem("authToken");
+  //   localStorage.removeItem("asHost");
+  //   this.setState({
+  //     authed: false,
+  //   });
+  // };
+
+  const renderContent = () => {
+    // if (!this.state.authed) {
+    //   return <LoginPage handleLoginSuccess={this.handleLoginSuccess} />;
+    // }
+    // if (this.state.asHost) {
+    //   return <HostHomePage />;
+    // }
+    // return <GuestHomePage />;
+  };
+
+  const userMenu = (
+    <Menu>
+      <Menu.Item key="logout" onClick={this.handleLogOut}>
+        Log Out
+      </Menu.Item>
+    </Menu>
   );
-}
+
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Header style={{ display: "flex", justifyContent: "space-between" }}>
+        <div style={{ fontSize: 16, fontWeight: 600, color: "white" }}>
+          Stays Booking
+        </div>
+        {this.state.authed && (
+          <div>
+            <Dropdown trigger="click" overlay={userMenu}>
+              <Button icon={<UserOutlined />} shape="circle" />
+            </Dropdown>
+          </div>
+        )}
+      </Header>
+      <Content
+        style={{ height: "calc(100% - 64px)", margin: 20, overflow: "auto" }}
+      >
+        {renderContent()}
+      </Content>
+    </Layout>
+  );
+};
 
 export default App;
